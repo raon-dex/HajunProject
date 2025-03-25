04a7b2a8b98caa031d95b7f0df4e2780a47e5ce3
raon-dex <whomi1104@naver.com>
2025-03-25 18:10:08 +0900

    start

---
+++ UIKitProject/UIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProject
+//
+//  Created by Mac on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitProject/UIKitProjectTests/UIKitProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitProjectTests.swift
+//  UIKitProjectTests
+//
+//  Created by Mac on 3/25/25.
+//
+
+import Testing
+@testable import UIKitProject
+
+struct UIKitProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
